4c62248b80e0fdb735dc4734696567d5b7c5c4c7
박철민 <118718073+fe26min@users.noreply.github.com>
2022-11-25 17:31:09 +0900

    TIL : 22_11_25
    til : 22_11_25

--- fastcampus/src/backjoon/sorting/ch03/Sort_pra/No11652.java
+++ fastcampus/src/backjoon/sorting/ch03/Sort_pra/No11652.java
@@ -3,49 +3,33 @@ package backjoon.sorting.ch03.Sort_pra;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class No11652 {
 	
-	static int N;
-	static HashSet<Point11652> arr;
-	
+	static long max;
+	static int maxC;
+	static HashMap<Long, Integer> hm;
 	public static void main(String[] args) throws IOException {
 		input();
+		System.out.println(max);
 	}
 
 	private static void input() throws IOException {
+		hm = new HashMap<>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new HashSet<Point11652>();
+		
+		int N = Integer.parseInt(br.readLine());
 		
 		for(int i=0; i<N; i++) {
 			long num = Long.parseLong(br.readLine());
 			
-			if(arr.contains(num)) {
-				arr.add(new Point11652(num, 1));
+			hm.put(num, hm.getOrDefault(num, 0) + 1);
+			if((hm.get(num) > maxC) || (hm.get(num) == maxC && num < max)) {
+				maxC = hm.get(num);
+				max = num;
 			}
 		}
-		
-		
 		br.close();
-	}
-}
-
-class Point11652 implements Comparable<Point11652>{
-	long num;
-	int count;
-	
-	public Point11652(long num, int count) {
-		this.num = num;
-		this.count = count;
-	}
-	
-	@Override
-	public int compareTo(Point11652 o) {
-		if(count == o.count) return num - o.num;
-		return o.count - count;
 	}
 }

4b88483e2508a4c53fd370557e6e41c2dd538a97
박철민 <118718073+fe26min@users.noreply.github.com>
2022-11-30 13:47:01 +0900

    TIL : BOJ 2667
    TIL : BOJ 2667

--- fastcampus/src/backjoon/fastcampus/ch06/GS_basic/No2667.java
+++ fastcampus/src/backjoon/fastcampus/ch06/GS_basic/No2667.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class No2667 {
 	

c695a7967d1744668d8265d61731f17f4dfc5c59
박철민 <118718073+fe26min@users.noreply.github.com>
2022-12-01 13:00:52 +0900

    TIL : BOJ 14502
    TIL : BOJ 14502

--- fastcampus/src/backjoon/fastcampus/ch06/GS_pra/No14502_BFS.java
+++ fastcampus/src/backjoon/fastcampus/ch06/GS_pra/No14502_BFS.java
@@ -3,6 +3,9 @@ package backjoon.fastcampus.ch06.GS_pra;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 // 4방 탐색 + 벽 세우기
@@ -12,12 +15,15 @@ import java.util.StringTokenizer;
 // 끝이 나면 숫자를 세준다.
 // 최대 크기를 찾는다.
 
-public class No14502 {
+public class No14502_BFS {
 	static int N, M, max=0;
 	static int[][] mat, copied;
 	static boolean[][] visit, isChecked;
+	static LinkedList<int[]> virus;
+	static Queue<int[]> queue;
 	static int[] dirx = {-1, 0, 1, 0};
 	static int[] diry = {0, -1, 0, 1};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,11 +31,14 @@ public class No14502 {
 		M = Integer.parseInt(st.nextToken());
 		mat = new int[N][M];
 		visit = new boolean[N][M];
+		virus = new LinkedList<int[]>();
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<M; j++) {
 				mat[i][j] = Integer.parseInt(st.nextToken());
+				if(mat[i][j] == 2)
+					virus.add(new int[] {i, j});
 			}
 		}
 		
@@ -67,12 +76,25 @@ public class No14502 {
 			copied[i] = mat[i].clone();
 		isChecked = new boolean[N][M];
 		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(isChecked[i][j] || copied[i][j]!=2) continue;
-				dfs(i, j);
+		queue = new LinkedList<int[]>();
+		queue.addAll(virus);
+		
+		while(!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			if(isChecked[cur[0]][cur[1]]) continue;
+			isChecked[cur[0]][cur[1]] = true;
+			
+			for(int di=0; di<4; di++) {
+				int nx = cur[0] + dirx[di];
+				int ny = cur[1] + diry[di];
+				if(nx<0 || ny<0 || nx>=N|| ny>=M) continue;
+				if(isChecked[nx][ny] || copied[nx][ny]==1) continue;
+				copied[nx][ny] = 2;
+				queue.add(new int[] {nx, ny});
 			}
 		}
+		
+		
 		int count = 0;
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {

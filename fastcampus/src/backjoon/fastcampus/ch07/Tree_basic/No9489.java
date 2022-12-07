235e152dfdb4c920d0b745b851e24193ec48cc58
fe26min <118718073+fe26min@users.noreply.github.com>
2022-12-07 21:55:53 +0900

    TIL : BOJ 9489
    TIL : BOJ 9489

--- fastcampus/src/backjoon/fastcampus/ch07/Tree_basic/No9489.java
+++ fastcampus/src/backjoon/fastcampus/ch07/Tree_basic/No9489.java
@@ -7,16 +7,50 @@ import java.util.StringTokenizer;
 
 //사촌 -> 부모 -> 부모 -> 자손 -> 자손
 public class No9489 {
-	
+
+	static int N,K,fidx;
+	static int[] arr, parent;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
-		
-		
-		
-		
-		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			if(N==0 && K==0) break;
+			
+			arr = new int[N + 1];
+			parent = new int[N + 1];
+			
+			st = new StringTokenizer(br.readLine());
+			
+			for(int i=1; i<=N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+				if(arr[i] == K)
+					fidx = i;
+			}
+			
+			parent[0] = -1;
+			parent[1] = 0;
+			
+			if(N>=2)
+				parent[2] = 1;
+			
+			int idx = 1;
+			
+			for(int i=3; i<=N; i++) {
+				if(arr[i-1] != arr[i]-1) idx++;
+				parent[i] = idx;
+			}
+			
+			int answer = 0;
+			for(int i=1; i<=N; i++) {
+				if(parent[parent[i]] == parent[parent[fidx]] && parent[i] != parent[fidx]) answer++;
+			}
+			System.out.println(answer);
+		}
 		br.close();
 	}
 }

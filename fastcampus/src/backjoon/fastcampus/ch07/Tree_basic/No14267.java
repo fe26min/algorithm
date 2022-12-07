f34f6dab5660d9ea0cbd5c7fdcf07c7f0f4e18f6
fe26min <118718073+fe26min@users.noreply.github.com>
2022-12-08 01:14:39 +0900

    TIL : BOJ 14267
    TIL : BOJ 14267

--- fastcampus/src/backjoon/fastcampus/ch07/Tree_basic/No14267.java
+++ fastcampus/src/backjoon/fastcampus/ch07/Tree_basic/No14267.java
@@ -3,16 +3,17 @@ package backjoon.fastcampus.ch07.Tree_basic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class No14267 {
-	static int N, M;
-	static int[] count, Tree;
-	static HashSet<Integer>[] child;
 	
+	static int N, M;
+	static int[] compliment, parent;
+	static ArrayList<Integer>[] child;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,41 +21,42 @@ public class No14267 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		count = new int[N];
-		Tree = new int[N];
-		child = new HashSet[N];
+		parent = new int[N+1];
+		child = new ArrayList[N+1];
+
+		compliment = new int[N+1];
 		
-		for(int i=0; i<N; i++)
-			child[i] = new HashSet<Integer>();
+		for(int i=1; i<=N; i++)
+			child[i] = new ArrayList<Integer>();
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			int p = Integer.parseInt(st.nextToken())-1;
-			if(p<0) continue;
-			Tree[i] = p;
+		for(int i=1; i<=N; i++) {
+			int p = Integer.parseInt(st.nextToken());
+			parent[i] = p;
+			if(p==-1) continue;
 			child[p].add(i);
 		}
 		
 		for(int idx=0; idx<M; idx++) {
 			st = new StringTokenizer(br.readLine());
-			int i = Integer.parseInt(st.nextToken()) - 1;
+			int i = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			
-			Queue<Integer> queue = new LinkedList<Integer>();
-			queue.add(i);
-			
-			while(!queue.isEmpty()) {
-				int cur = queue.poll();
-				count[cur] += w;
-				for(int num : child[cur]) {
-					queue.add(num);
-				}
-			}
-		}
-		for(int i: count) {
-			System.out.print(i + " ");
+			compliment[i] += w;
 		}
 		
+		// 부모 부터 차근 차근 넘겨 주기
+		dfs(1);
+		
+		for(int i = 1; i<=N; i++)
+			System.out.print(compliment[i] + " ");
 		br.close();
+	}
+	
+	private static void dfs(int idx) {
+		for(int c : child[idx]) {
+			compliment[c] += compliment[idx];
+			dfs(c);
+		}
 	}
 }

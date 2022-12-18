c03d48ef4a1522f596e7989d3554e4f9cd262f40
fe26min <118718073+fe26min@users.noreply.github.com>
2022-12-18 16:18:23 +0900

    TIL : BOJ 1637
    TIL : BOJ 1637

--- fastcampus/src/backjoon/answer_note/No1637.java
+++ fastcampus/src/backjoon/answer_note/No1637.java
@@ -6,67 +6,91 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class No1637 {
-	
-	static int N;
-	static int[] A, B, C;
-	
-	static long min = Long.MAX_VALUE;
-	static long max = Long.MIN_VALUE;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		
-		A = new int[N];
-		B = new int[N];
-		C = new int[N];
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			A[i] = Integer.parseInt(st.nextToken());
-			C[i] = Integer.parseInt(st.nextToken());
-			B[i] = Integer.parseInt(st.nextToken());
-			
-			min = Math.min(min, A[i]);
-			max = Math.max(max, C[i]);
-		}
-		max++;
-		bs();
-		br.close();
-	}
 
-	private static void bs() {
-		long l = min;
-		long r = max;
-		
-		while(l < r) {
+		int N = Integer.parseInt(br.readLine());
+
+		int arr[][] = new int[N][3];
+
+		// l의 범위를 정하기 위한 A값의 최소를 저장할 곳
+		int minA = Integer.MAX_VALUE;
+		// r의 범위를 정하기 위한 C값의 최대를 저장할 곳
+		int maxC = 0;
+
+		// N줄에 걸쳐 A, C, B를 입력받는다.
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int C = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+
+			// A, B, C 저장
+			arr[i][0] = A;
+			arr[i][1] = B;
+			arr[i][2] = C;
+
+			// 최대 최소 기록하기
+			minA = Math.min(minA, A);
+			maxC = Math.max(maxC, C);
+
+		}
+		br.close();
+
+		long l = minA;
+		long r = maxC;
+
+		// 이분 탐색 시작
+		while (l < r) {
 			long mid = (l + r) / 2;
-			
-			long sum = getSum(mid);
-			
-			if(sum % 2 ==0) {
-				l = mid+1;
+
+			// A,B,C 목록들을 보면서 mid 보다 작은 숫자들의 갯수가 기록되는 곳
+			long count = 0;
+
+			// A,B,C 목록 돌기
+			for (int i = 0; i < N; i++) {
+				int A = arr[i][0];
+				int B = arr[i][1];
+				int C = arr[i][2];
+
+				if (mid < A) continue;
+				count += (Math.min(C, mid) - A) / B + 1;
 			}
-			else {
+//			System.out.println(l + " " + mid + " " + count);
+			// count가 짝수이면 갯수가 홀수인 수는 mid에서 r사이에 있다.
+			if (count % 2 == 0) {
+				l = mid + 1;
+			}
+			// count가 홀수이면 갯수가 홀수인 수는 l과 mid사이에 있다.
+			else if (count % 2 == 1) {
 				r = mid;
 			}
 		}
-		
-		if(l == max) 
+
+		// l의 갯수를 찾아줘야 한다.
+		// A + i * B == l일 경우를 세준다.
+		// 다만 l이 C보다 클 경우, A가 l보다 클 경우는 계산 X
+		// (l - A) % B == 0일 경우를 세준다.
+		long count = 0;
+		// A,B,C 목록 돌기
+		for (int i = 0; i < N; i++) {
+			int A = arr[i][0];
+			int B = arr[i][1];
+			int C = arr[i][2];
+			
+			// 범위를 벗어난다면 
+			if (C < l || l < A)
+				continue;
+			if ((l - A) % B == 0)
+				count++;
+		}
+		// maxN까지 갔을 경우 만약 maxN이 짝수이면 NOTHING이다.
+		if (l == maxC && count % 2 == 0) {
 			System.out.println("NOTHING");
-		else {
-			long num = getSum(l) - getSum(l-1);
-			System.out.println(l + " " + num);
+		} else {
+			System.out.println(l + " " + count);
 		}
-	}
-	
-	static long getSum(long mid) {
-		long sum = 0;
-		for(int i=0; i<N; i++) {
-			if(mid >= A[i])
-				sum += (Math.min(mid, C[i]) - A[i]) / B[i] + 1;
-		}
-		return sum;
 	}
 }

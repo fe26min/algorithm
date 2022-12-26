df4dd718aa5f167988acecf572430224039da38e
fe26min <118718073+fe26min@users.noreply.github.com>
2022-12-26 20:21:21 +0900

    TIL : AnswerNote BOJ 9489
    TIL : AnswerNote BOJ 9489

--- AnswerNote/src/backjoon/answer_note/No9489.java
+++ AnswerNote/src/backjoon/answer_note/No9489.java
@@ -3,65 +3,67 @@ package backjoon.answer_note;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class No9489 {
 	
-	static int n;
-	static int[] parent;
-	static ArrayList<Integer> child[];
-	static StringBuilder sb;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
+		
 		while(true) {
 			st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
+			
+			// n, k 입력 받기
+			int n = Integer.parseInt(st.nextToken());
 			int k = Integer.parseInt(st.nextToken());
-			if(n == 0 && k == 0) break;
+			if(n==0 && k==0) break;
 			
-			// 부모를 가르키는 배열
-			parent = new int[n+1];
-			// 자식을 가르키는 배열
-			child = new ArrayList[n+1];
-			
-			for(int i=0; i<=n; i++)
-				child[i] = new ArrayList<Integer>();
-			
+			// 배열 입력 받기
+			int[] arr = new int[n];
+			// 부모 저장
+			int[] parent = new int[n];
+			// 맨 위의 노트는 -1
+			parent[0] = -1;
 			st = new StringTokenizer(br.readLine());
 			
-			parent[0] = -1;
-			int p = Integer.parseInt(st.nextToken());
-			int pidx = 0;
-			int kidx = -1;
+			// 찾고자 하는 k 위치
+			int fidx = -1;
 			
-			for(int i=2; i<=n; i++) {
-				int num = Integer.parseInt(st.nextToken());
-				if(num != p + 1) pidx++;
-				p = num;
-				parent[i] = pidx;
-				child[pidx].add(i);
-				if(num == k) kidx = i;
+			// 배열 입력
+			for(int i=0; i<n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+				if(k==arr[i]) fidx = i;
 			}
 			
-			// 사촌의 수를 세는 문제
-			pro(kidx);
+			// 배열의 크기가 1 초과일 경우!
+			// 1번째는 0을 가르키고 있다.
+			// 입력이 만약 1 2 3 이런 식으로 들어와도 1은 2와 3의 부모가 되어야 한다
+			if(n > 1) {
+				parent[1] = 0;
+			}
+			// 부모의 위치 (연속이 아니면 하나씩 이동)
+			int pidx = 0;
 			
+			// 2번째부터 보기
+			for(int i=2; i<n; i++) {
+				if(arr[i-1] != arr[i] - 1)
+					pidx++;
+				parent[i] = pidx;
+			}
+			int count = 0;
+			
+			// 배열을 돌면서 부모가 다르면서 부모의 부모가 같은 것들을 세줍니다.
+			for(int i=0; i<n; i++) {
+				if(parent[i]==-1 || parent[parent[i]] == -1) continue;
+				if(parent[i]!=parent[fidx] && parent[parent[i]] == parent[parent[fidx]]) count++;
+			}
+			// sb에 저장
+			sb.append(count +"\n");
 		}
+		// sb 출력
 		System.out.println(sb);
 		br.close();
-	}
-
-	private static void pro(int kidx) {
-		int count = 0;
-		
-		for(int i=1; i<=n; i++) {
-			if(parent[parent[i]] == parent[parent[kidx]]) {
-				if(parent[i] != parent[kidx]) count++;
-			}
-		}
-		sb.append(count + "\n");
 	}
 }

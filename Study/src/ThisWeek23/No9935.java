00d09ff16014d4263db2df2cc9a055255ace2b63
fe26min <118718073+fe26min@users.noreply.github.com>
2023-01-09 23:02:54 +0900

    TIL : study
    TIL : study

---
+++ Study/src/ThisWeek23/No9935.java
@@ -0,0 +1,20 @@
+package ThisWeek23;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No9935 {
+	public static void main(String[] args) throws IOException {
+		input();
+		
+	}
+
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder answer = new StringBuilder();
+		String line = br.readLine();
+		
+		br.close();
+	}
+}

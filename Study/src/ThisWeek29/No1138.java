c1efd0afaeb5f30febe35ef57cbeb5768768613d
fe26min <118718073+fe26min@users.noreply.github.com>
2023-02-24 19:08:50 +0900

    TIL :STUDY
    TIL :STUDY

---
+++ Study/src/ThisWeek29/No1138.java
@@ -0,0 +1,51 @@
+package ThisWeek29;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class No1138 {
+	static int N;
+	static LinkedList<Integer> list;
+	static int[] arr, ans;
+	
+	public static void main(String[] args) throws IOException {
+		input();
+		pro();
+	}
+
+	private static void pro() {
+		ans = new int[N];
+		list = new LinkedList<>();
+		
+		for(int i=0; i<N; i++) {
+			list.add(i);
+		}
+		for(int i=0; i<N; i++) {
+			ans[list.get(arr[i])] = i+1;
+			list.remove(arr[i]);
+		}
+		
+		for(int i: ans) {
+			System.out.print(i + " ");
+		}
+	}
+
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		br.close();
+	}
+}
---
+++ Study/src/ThisWeek29/No2668.java
@@ -0,0 +1,67 @@
+package ThisWeek29;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class No2668 {
+	static int N, count;
+	static int[] parent;
+	static boolean[] visit, finish, isCycle;
+	static ArrayList<Integer> list;
+	
+	public static void main(String[] args) throws IOException {
+		input();
+		pro();
+	}
+
+	private static void pro() {
+		count =0;
+		visit = new boolean[N];
+		finish = new boolean[N];
+		isCycle = new boolean[N];
+		
+		for(int i=0; i<N; i++) {
+			dfs(i);
+		}
+		System.out.println(count);
+		for(int i=0; i<N; i++) {
+			if(isCycle[i])
+				System.out.println(i+1);
+		}
+	}
+
+	private static void dfs(int cur) {
+		if(visit[cur])
+			return;
+		
+		visit[cur] = true;
+		int next = parent[cur];
+		
+		if(!visit[next])
+			dfs(next);
+		if(visit[next] && !finish[next]) {
+			isCycle[cur] = true;
+			count++;
+			for(int i=next; i != cur; i = parent[i]) {
+				isCycle[i] = true;
+				count++;
+			}
+		}
+		dfs(next);
+		finish[cur] = true;
+	}
+
+	private static void input() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		parent = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			parent[i] = Integer.parseInt(br.readLine()) - 1;
+		}
+		br.close();
+	}
+}

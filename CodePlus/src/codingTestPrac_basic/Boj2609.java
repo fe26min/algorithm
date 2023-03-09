c551c37ace5eacd5106d44b2b7988bc806164cc4
fe26min <118718073+fe26min@users.noreply.github.com>
2023-03-09 18:43:15 +0900

    BOj 2609 : 최대공약수와 최소공배수
    BOj 2609 : 최대공약수와 최소공배수

---
+++ CodePlus/src/codingTestPrac_basic/Boj2609.java
@@ -0,0 +1,31 @@
+package codingTestPrac_basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj2609 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int max = Math.max(a, b);
+		int min = Math.min(a, b);
+		
+		while(max % min != 0) {
+			max -= min;
+			if(max < min) {
+				int tmp = min;
+				min = max;
+				max = tmp;
+			}
+		}
+		System.out.println(min);
+		System.out.println(a * b / min);
+		
+		br.close();
+	}
+}

9afb63e7dd147eead07680210d846f1a8021b31b
fe26min <118718073+fe26min@users.noreply.github.com>
2023-03-09 17:53:20 +0900

    BOJ 4367 : 1
    BOJ 4367 : 1

---
+++ CodePlus/src/codingTestPrac_basic/Boj4375.java
@@ -0,0 +1,24 @@
+package codingTestPrac_basic;
+
+import java.util.Scanner;
+
+public class Boj4375 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			int N = sc.nextInt();
+			int num = 0;
+			int idx = 1;
+			while(true) {
+				num = num * 10 + 1;
+				num %= N;
+				if(num == 0) {
+					System.out.println(idx);
+					break;
+				}
+				idx++;
+			}
+		}
+	}
+}

6eae8cd04e3479861dff299062a9ebe05f0f703f
fe26min <118718073+fe26min@users.noreply.github.com>
2023-04-17 19:46:17 +0900

    BOJ 10973 : 이전 수열
    BOJ 10973 : 이전 수열

---
+++ Study/src/ThisWeek38/No10973.java
@@ -0,0 +1,72 @@
+package ThisWeek38;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No10973 {
+	static int N;
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		input();
+		pro();
+	}
+	private static void pro() {
+		StringBuilder sb = new StringBuilder();
+		if(check()) {
+			for(int i : arr) {
+				sb.append(i + " ");
+			}
+		}
+		else
+			sb.append(-1);
+		System.out.println(sb);
+	}
+	
+	private static boolean check() {
+		// 규칙 1. 앞에가 뒤에 보다 큰 수 찾기
+		int i = N-1;
+		
+		while(i>0 && arr[i-1] < arr[i])
+			i--;
+		// i가 0이라면 모든 배열이 오름차순이다.
+		// 즉 배열 중에 가장 작은 수이다.
+		if(i==0)
+			return false;
+			
+		int j = N-1;
+		
+		// 규칙 2 arr[i-1]보다 작은 수 중에 가장 뒤에 있는 수(가장 큰 수)를 찾기
+		while(arr[j] >= arr[i-1]) {
+			j--;
+		}
+		swap(i-1, j);
+		
+		// 규칙 3. 오름차순으로 가장 작은 수이기 때문에 내림차순으로 가장 큰 수로 바꾸기
+		j = N-1;
+		while(i < j) {
+			swap(i, j);
+			i++;
+			j--;
+		}
+		return true;
+	}
+	
+	private static void swap(int j, int i) {
+		int temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+	}
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		br.close();		
+	}
+}

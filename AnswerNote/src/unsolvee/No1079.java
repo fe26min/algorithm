06ce95f493d4b1243b1d865459a9bb26636a663e
fe26min <118718073+fe26min@users.noreply.github.com>
2023-05-08 21:02:23 +0900

    [BOJ 1079] 마피아
    [BOJ 1079] 마피아

--- AnswerNote/src/unsolvee/No1079.java
+++ AnswerNote/src/unsolvee/No1079.java
@@ -6,38 +6,82 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class No1079 {
-	static int N, idx;
-	static int[] R;
-	static int[][] mat;
+	static int N, max, enjin;
+	static int[] guilty;
+	static int[][] R;
+
+	static boolean[] isDead;
+
 	public static void main(String[] args) throws IOException {
 		input();
 		pro();
 	}
+
 	private static void pro() {
-		int ans = 0;
-		
-		System.out.println(ans);
+		isDead = new boolean[N];
+		max = 0;
+		dfs(0, N);
+		System.out.println(max);
 	}
-	private static void input() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		R = new int[N];
-		mat = new int[N][N];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			R[i] = Integer.parseInt(st.nextToken());
+
+	private static void dfs(int depth, int num) {
+		if (num == 1 || isDead[enjin]) {
+			max = Math.max(max, depth);
+			return;
 		}
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				mat[i][j] = Integer.parseInt(st.nextToken());
+
+		// 밤
+		if (num % 2 == 0) {
+			for(int i = 0; i < N; i++) {
+				if (i == enjin || isDead[i])
+					continue;
+
+				isDead[i] = true;
+				for (int j = 0; j < N; j++)
+					guilty[j] += R[i][j];
+				dfs(depth + 1, num - 1);
+				isDead[i] = false;
+				for (int j = 0; j < N; j++)
+					guilty[j] -= R[i][j];
 			}
 		}
-		idx = Integer.parseInt(br.readLine());
+		// 낮
+		else {
+			int susNum = -1;
+			int susIdx = -1;
+			
+			for(int i=0; i<N; i++) {
+				if(!isDead[i] && susNum < guilty[i]) {
+					susNum = guilty[i];
+					susIdx = i;
+				}
+			}
+			
+			isDead[susIdx] = true;
+			dfs(depth, num-1);
+			isDead[susIdx] = false;
+		}
+	}
+
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		guilty = new int[N];
+		R = new int[N][N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			guilty[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				R[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		enjin = Integer.parseInt(br.readLine());
 		br.close();
 	}
 }

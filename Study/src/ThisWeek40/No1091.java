a58ff8268fb8f23fc7129d08e3b5a2b5e78da7e0
fe26min <118718073+fe26min@users.noreply.github.com>
2023-05-14 13:52:53 +0900

    [BOJ 1091] 카드 섞기
    [BOJ 1091] 카드 섞기

---
+++ Study/src/ThisWeek40/No1091.java
@@ -0,0 +1,78 @@
+package ThisWeek40;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class No1091 {
+	static int N;
+	static int[] P, S, arr;
+	public static void main(String[] args) throws IOException {
+		input();
+		pro();
+	}
+	
+	private static void pro() {
+		arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = i % 3;
+		}
+		int cnt = 0;
+		
+		while(true) {
+			if(check())
+				break;
+			shuffle();
+			cnt++;
+			if(dormamu()) {
+				cnt = -1;
+				break;
+			}
+		}		
+		System.out.println(cnt);
+	}
+	
+	private static boolean dormamu() {
+		for(int i=0; i<N; i++) {
+			if(arr[i] != i%3)
+				return  false;
+		}
+		return true;
+	}
+
+	private static boolean check() {
+		for(int i=0; i<N; i++) {
+			if(arr[i] != P[i])
+				return false;
+		}
+		return true;
+	}
+
+	private static void shuffle() {
+		int[] temp = new int[N];
+		for(int i=0; i<N; i++) {
+			temp[i] = arr[S[i]];
+		}
+		arr = temp;
+	}
+
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		P = new int[N];
+		S = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) 
+			P[i] = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) 
+			S[i] = Integer.parseInt(st.nextToken());
+		
+		br.close();		
+	}
+}
